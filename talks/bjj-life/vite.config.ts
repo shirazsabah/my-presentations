71ba04d371b85ab5e95a7e395c80252e8e0fcb13
Shiraz Sabah <68498257+shirazsabah@users.noreply.github.com>
2025-05-01 19:33:48 +1000

    Update vite.config.ts

--- talks/bjj-life/vite.config.ts
+++ talks/bjj-life/vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
-  base: '/my-presentations/talks/bjj-life/',
+  plugins: [
+    Components({
+      resolvers: [IconsResolver({ prefix: 'Icon' })], // optional: allows <IconMdiShuffleVariant />
+    }),
+    Icons({
+      autoInstall: true, // installs icon sets as needed
+    }),
+  ],
 })
